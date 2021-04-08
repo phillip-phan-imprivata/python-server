50d39d49949b336c08be45d05082e378b8e4468d
phillipphan <itsphillipphan@gmail.com>
2021-04-08 10:37:19 -0500

    first commit

---
+++ employees/request.py
@@ -0,0 +1,25 @@
+EMPLOYEES = [
+  {
+    "id": 1,
+    "name": "Bruh Bro",
+    "locationId": 1
+  },
+  {
+    "id": 2,
+    "name": "What Guy",
+    "locationId": 1
+  },
+  {
+    "id": 3,
+    "name": "Good Worker",
+    "locationId": 2
+  },
+  {
+    "id": 4,
+    "name": "George George",
+    "locationId": 2
+  }
+]
+
+def get_all_employees():
+    return EMPLOYEES
