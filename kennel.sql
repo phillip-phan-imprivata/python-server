ec5867e099005c2d473bd7fa7ee4b9f07e97275a
phillipphan <itsphillipphan@gmail.com>
2021-04-21 09:36:11 -0500

    animals can be added

--- kennel.sql
+++ kennel.sql
@@ -76,3 +76,38 @@ SELECT
       a.password
     FROM customer a
     WHERE a.id = 2
+
+SELECT
+    a.id,
+    a.name,
+    a.breed,
+    a.status,
+    a.location_id,
+    a.customer_id,
+    l.name location_name,
+    l.address location_address
+FROM Animal a
+JOIN Location l
+    ON l.id = a.location_id
+
+SELECT
+    a.id,
+    a.name,
+    a.breed,
+    a.status,
+    a.location_id,
+    a.customer_id,
+    l.name location_name,
+    l.address location_address,
+    c.name customer_name,
+    c.address customer_address,
+    c.email customer_email,
+    c.password customer_password
+FROM Animal a
+JOIN Location l
+    ON l.id = a.location_id
+JOIN Customer c
+    ON c.id = a.customer_id
+
+SELECT *
+FROM Animal
